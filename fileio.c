293df932a7b78c4f96b93f428ca97f6aa90076eb
Sam Sherar <s.sherar@live.com>
2012-12-07 03:17:32 +0000

    clean up on aisle 3

--- fileio.c
+++ fileio.c
@@ -68,48 +68,6 @@ void load_node_file(char filename[], int lines, Node *node_types) {
     fclose(fp);
 }
 
-/*void load_courses_file(char filename[], int lines, Course *course, 
-        Node *node_types, Competitor *comp, int comp_lines) {
-    FILE* fp;
-    int i = 0, j = 0, amount_nodes = 0, comp_index = 0;
-    char id;
-    fp = fopen(filename, "r");
-    for (i = 0; i < lines; i++) {
-        fscanf(fp, "%c %d", &id, &amount_nodes);
-        course[i].id_name = id;
-        course[i].length = amount_nodes;
-        course[i].head = NULL;
-        //strcpy(course[i].start_time, "NULL");
-        strcpy(course[i].end_time, "NULL");
-        for(comp_index = 0; comp_index < comp_lines; comp_index++) {
-            int val = 0;
-            Course_Node *tmp;
-            tmp = malloc(sizeof (Course_Node));
-            fscanf(fp, " %d", &val);
-            if(course[i].id_name == comp[comp_index].course_id) {
-                comp[comp_index].course = course[i];
-                for (j = 0; j < amount_nodes; j++) {
-                    
-                    strcpy(tmp->type, node_types[val].type);
-                    tmp->time[0] = NULL;
-                    tmp->node_id = val;
-                    tmp->next = NULL;
-                    if (0 == j) {
-                        course[i].head = tmp;
-                        course[i].start_time = &tmp->time; 
-                    } else {
-                        insert_node((Course_Node*) course[i].head, tmp);
-                    }
-                    fscanf(fp, "\n");
-                }
-            }
-        }
-        
-        
-    }
-    fclose(fp);
-}*/
-
 void load_courses_file(char filename[], int lines,
         Node *node_types, Competitor *comp, int comp_lines) {
     FILE* fp;
