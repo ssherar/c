5cacf484c02795625aae4ad916d988534f5b9a8d
Sam Sherar <s.sherar@live.com>
2012-12-08 16:49:09 +0000

    Commenting node.c

--- node.c
+++ node.c
@@ -1,9 +1,31 @@
-/** node.c */
+/*
+ * =====================================================================================
+ *
+ *       Filename:  node.c
+ *
+ *    Description:  Functions for manipulating linked list data structures
+ *
+ *        Version:  1.0
+ *        Created:  12/01/2012 11:43:13 PM
+ *       Revision:  none
+ *       Compiler:  gcc -Wall
+ *
+ *         Author:  Samuel Sherar (sbs1), sbs1@aber.ac.uk
+ *        Company:  Aberystwyth University
+ *
+ * =====================================================================================
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "data_struct.h"
 
+/**
+ * insert a node at the end of the list recursively.
+ * @param current the current node
+ * @param value the node to be inserted
+ */
 void insert_node(Course_Node* current, Course_Node* value) {
     if (current == NULL) {
         current = value;
@@ -14,8 +36,13 @@ void insert_node(Course_Node* current, Course_Node* value) {
     }
 }
 
+/**
+ * Find a node with a certain amount node identifier recursively
+ * @param current the current node to be checked
+ * @param node_id the node identifier
+ * @return a pointer to the found node, NULL if not found
+ */
 Course_Node* find_node(Course_Node* current, int node_id) {
-    
     if(current->node_id == node_id) {
         return current;
     } else {
@@ -24,14 +51,25 @@ Course_Node* find_node(Course_Node* current, int node_id) {
         }
         return find_node(current->next, node_id);
     }
-   
 }
 
+/**
+ * An entry point to the find_node(current, id) when the parameter
+ * is a pointer to a Course_Node pointer.
+ * @param head the head of the list(as a Pointer to a Pointer)
+ * @param node_id the node identifier
+ * @return a pointer to the found node, NULL if not found
+ */
 Course_Node* find_node_head(Course_Node** head, int node_id) {
     return find_node((Course_Node*) head, node_id);
 }
 
-
+/**
+ * Find the next node which is empty which has a certain identifier
+ * @param head the head of the list (which is a pointer to a pointer)
+ * @param id the node identifier
+ * @return a pointer to the found node, NULL if not found.
+ */
 Course_Node* check_next_empty(Course_Node** head, int id) {
     Course_Node* current = (Course_Node*) head;
     while(current != NULL) {
@@ -45,6 +83,11 @@ Course_Node* check_next_empty(Course_Node** head, int id) {
     return NULL;
 }
 
+/**
+ * Finds the last node which is not empty
+ * @param head the head of the list
+ * @return a pointer to the found node, NULL if not found
+ */
 Course_Node* find_current_node(Course_Node** head) {
     Course_Node* current = (Course_Node*) head;
     Course_Node* prev_cp;
@@ -62,6 +105,10 @@ Course_Node* find_current_node(Course_Node** head) {
     return NULL;
 }
 
+/**
+ * A debug function which prints out the list
+ * @param head the head of the list
+ */
 void print_list(Course_Node** head) {
     Course_Node* current = (Course_Node*) head;
     while (current != NULL) {
@@ -70,27 +117,36 @@ void print_list(Course_Node** head) {
     }
 }
 
-
+/**
+ * A method which stores the data passed in from the file in the next 
+ * empty node available
+ * @param head the head of the list
+ * @param comp_id the competitor id
+ * @param checkpoint_id the checkpoint identifier
+ * @param time the time
+ */
 void insert_checkpoint_data(Course** head, int comp_id, int checkpoint_id, char *time) {
     Course_Node* found = (Course_Node*) find_node_head(head, checkpoint_id);
     Course_Node* next_empty = (Course_Node*) check_next_empty(head, checkpoint_id);
     if(found != NULL && next_empty != NULL) {
-        //if(found == next_empty) {
-            strcpy(next_empty->time, time);
-            printf("Node %d was hit at %s by competitor %d\n",
-                    next_empty->node_id,
-                    next_empty->time,
-                    comp_id);
-        /*} else {
-            printf("Not at correct checkpoint for COMP %d at NODE %d\n",
-                    comp_id, checkpoint_id);
-        }*/
+        strcpy(next_empty->time, time);
+        printf("Node %d was hit at %s by competitor %d\n",
+                next_empty->node_id,
+                next_empty->time,
+                comp_id);
     } else {
-        printf("Error: cannot find any empty times for Competitor %d\n",
+        printf("Error: cannot find any empty times for Competitor %d \n",
                 comp_id);
     }
 }
 
+/**
+ * A method to insert the data manually
+ * @param head the head of the list
+ * @param comp_id the competitor id
+ * @param checkpoint_id the checkpoint identifier
+ * @param time the time
+ */
 void insert_checkpoint_data_manually(Course** head, int comp_id, int checkpoint_id, char time[]) {
     Course_Node* found = (Course_Node*) find_node_head(head, checkpoint_id);
     if(found != NULL) {
